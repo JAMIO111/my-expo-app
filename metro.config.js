e66f3a37f958bb3d2a160f949fc9b5d2b97a8315
JAMIO111 <drydenjamie@hotmail.com>
2026-05-16 21:13:49 +0100

    package updates

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(process.cwd());
 
-// Add your aliases
-config.resolver.alias = {
-  '@': './app',
-  '@components': './app/components',
-  '@assets': './app/assets',
-  '@hooks': './app/hooks',
-  '@lib': './app/lib',
-  '@contexts': './app/contexts',
-};
-
-// Exclude prettier folder from bundling
-config.resolver.blacklistRE = exclusionList([/prettier\/.*/]);
-
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativeWind(config, {
+  input: './global.css',
+});
